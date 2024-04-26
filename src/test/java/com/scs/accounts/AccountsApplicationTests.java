ba03194be8e88ec26552629361d4c71261ed82e9
Supriya CS <supriyasubramanian92@gmail.com>
2024-04-26 09:35:42 +0530

    first commit

---
+++ src/test/java/com/scs/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scs.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
